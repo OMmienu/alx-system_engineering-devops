ab99c3828443cc70b7b60c88bb228988a256ad9e
OMmienu <nkedzi@gmail.com>
2023-05-09 15:31:01 -0700

    2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -6,7 +6,7 @@ import requests
 def recurse(subreddit, hot_list=[], after=None):
     """return list of all hot posts titles of a subreddit"""
 
-    headers = {"User-agent': 'my agent'}
+    headers = {'User-agent': 'my agent'}
     url = "https://www.reddit.com/r/{}/hot.json?limit=50&after={}".format(
             subreddit, after)
     posts = requests.get(url, headers=headers, allow_redirects=False)
