ea41934260d65b285ad42aa4dd37a12b79db3b2b
OMmienu <nkedzi@gmail.com>
2023-05-09 16:27:15 -0700

    100-count.py

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -11,7 +11,7 @@ def count_words(subreddit, word_list, after="", count=[]):
     if after == "":
         count = [0] * len(word_list)
 
-    url = "https://www.reddit.com/r/{}/json".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     request = requests.get(url,
                            params={'after': after},
                            allow_redirects=False,
@@ -21,12 +21,12 @@ def count_words(subreddit, word_list, after="", count=[]):
         data = request.json()
 
         for topic in (data['data']['children']):
-            fro word in topic['data']['title'].split():
+            for word in topic['data']['title'].split():
                 for i in range(len(word_list)):
                     if word_list[i].lower() == word.lower():
                         count[i] += 1
 
-        after data['data']['after']
+        after = data['data']['after']
         if after is None:
             save = []
             for i in range(len(word_list)):
@@ -47,7 +47,7 @@ def count_words(subreddit, word_list, after="", count=[]):
                         word_list[i] = word_list[j]
                         word_list[j] = aux
 
-            for i range(len(word_list)):
+            for i in range(len(word_list)):
                 if (count[i] > 0) and i not in save:
                     print("{}: {}".format(word_list[i].lower(), count[i]))
         else:
