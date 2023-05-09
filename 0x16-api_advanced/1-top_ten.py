01779a208f97fff604565573227857783b4722e5
OMmienu <nkedzi@gmail.com>
2023-05-09 14:52:19 -0700

    1-top_ten.py

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""A Script that queries the REDDIT API and prints 1st 10 hot post"""
+import requests
+
+
+def top_ten(subreddit):
+    """retrieve top 10 hot post per subreddit"""
+
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = {"User-Agent": 'My agent'}
+    response = request.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        posts = response.json()
+
+        for post in posts['data']['children']:
+            print(post['data']['title'])
+    else:
+        print(None)
